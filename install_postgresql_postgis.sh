a8cb22c467cb6335a8ac3d816a1e88903e724c2b
aaccportal <aaccportal@gmail.com>
2025-03-26 11:30:55 +0300

    Установка PostgreSQL с расширением PostGIS

---
+++ install_postgresql_postgis.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Обновление списка пакетов
+sudo apt update
+
+# Установка PostgreSQL и PostGIS
+sudo apt install -y postgresql postgresql-contrib postgis
+
+# Настройка пароля для пользователя postgres
+sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'toor';"
+
+# Создание базы данных для PostGIS
+sudo -u postgres createdb -E UTF8 template_postgis
+
+# Установка расширения PostGIS в базе данных template_postgis
+sudo -u postgres psql -d template_postgis -c "CREATE EXTENSION postgis;"
+
+# Проверка установки PostgreSQL
+psql --version
+
+# Проверка установки PostGIS
+psql -d template_postgis -c "SELECT postgis_full_version();"
