1f05e6e7b091ab0d4598051a62ab1cb2982be173
aaccportal <aaccportal@gmail.com>
2025-03-26 15:23:17 +0300

    Установка dotnet

--- install_dotnet.sh
+++ install_dotnet.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
-# Обновление списка пакетов
+# Обновляем список пакетов
 sudo apt update
 
-# Установка зависимостей
+# Устанавливаем зависимости
 sudo apt install -y wget apt-transport-https
 
-# Добавление ключа репозитория Microsoft GPG
-wget https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+# Добавляем ключ репозитория Microsoft
+wget https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
 
-# Добавление репозитория .NET
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/ubuntu/$(lsb_release -rs)/prod $(lsb_release -cs) main" > /etc/apt/sources.list.d/dotnet.list'
-
-# Обновление списка пакетов с новым репозиторием
+# Обновляем список пакетов после добавления репозитория
 sudo apt update
 
-# Установка .NET SDK 9
+# Устанавливаем .NET SDK 9
 sudo apt install -y dotnet-sdk-9
 
-# Установка ASP.NET Core Runtime 9
+# Устанавливаем ASP.NET Core Runtime 9
 sudo apt install -y aspnetcore-runtime-9
 
-# Установка .NET Runtime 9
+# Устанавливаем .NET Runtime 9
 sudo apt install -y dotnet-runtime-9
 
-# Проверка установки
+# Проверяем установку
 dotnet --version
