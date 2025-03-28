07027d46fda31db65ec1aab7af3044a31e44f71d
aaccportal <aaccportal@gmail.com>
2025-03-28 08:51:25 +0300

    Активация окружения general_dev_env

---
+++ activate_general_dev_env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Активация окружения для общей разработки приложений
+echo "Активация окружения general_dev_env..."
+source activate general_dev_env
+echo "Окружение general_dev_env активировано."
