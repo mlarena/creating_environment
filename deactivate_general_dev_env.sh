a75c2b0875e1b6c358621c16ee83605ccb20eb2e
aaccportal <aaccportal@gmail.com>
2025-03-28 08:52:24 +0300

    Деактивация окружения general_dev_env

---
+++ deactivate_general_dev_env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Деактивация окружения для общей разработки приложений
+echo "Деактивация окружения general_dev_env..."
+conda deactivate
+echo "Окружение general_dev_env деактивировано."
