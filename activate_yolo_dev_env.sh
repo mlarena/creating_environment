e4fe54f815adb520a5fbeb0ccec2aabd71dc3d02
aaccportal <aaccportal@gmail.com>
2025-03-28 08:51:52 +0300

    Активация окружения yolo_dev_env

---
+++ activate_yolo_dev_env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Активация окружения для разработки YOLO решений
+echo "Активация окружения yolo_dev_env..."
+source activate yolo_dev_env
+echo "Окружение yolo_dev_env активировано."
